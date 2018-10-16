d5a5be400d2e779d7cf5d2edd5facc330f4b3950
PieroKom1972 <pierokaczmarek@gmail.com>
2018-10-16 20:08:32 +0200

    Add project files.

---
+++ siatkowka/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(siatkowka.Startup))]
+namespace siatkowka
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
